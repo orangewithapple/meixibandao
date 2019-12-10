5f47a756b27b354887fc0d3151024d6990293d80
orangewithapple <527213808@qq.com>
2019-12-10 21:40:36 +0800

    新增子菜单

--- web/meixiWeb/src/components/businessDetail.vue
+++ web/meixiWeb/src/components/businessDetail.vue
@@ -1,24 +1,26 @@
 <template>
     <div :style="{height:phoneHeight}" class="detailBox">
-        <div class="headImage">
-            <i class="iconfont icon-return back" @click="back"></i>
-            <p class="vipDiscount"><i class="iconfont icon-xingxing"></i>{{headDiscount}}</p>
-            <van-image :src="headImage"/>
-        </div>
-        <div class="introduce column-flex-start">
-            <p  v-for="(item,index) in introduceList" :key="index">{{item}}</p>
-        </div>
-        <div class="discountDetail">
-            <h1>【优惠详情】</h1>
-            <p v-for="(item,index) in discountDetailList" :key="index"><i class="circular">{{index+1}}</i>{{item}}</p>
-        </div>
+            <van-swipe :autoplay="3000" indicator-color="white">
+                <van-swipe-item v-for="(item,index) in list" :key="index" @click="changeBtn(index)">
+                    <!-- <i class="iconfont icon-return back" @click="back"></i> -->
+                    <p class="vipDiscount"><i class="iconfont icon-xingxing"></i>{{item.discount}}</p>
+                    <van-image :src="item.url"/>
+                </van-swipe-item>
+            </van-swipe>
+            <div class="introduce column-flex-start">
+                <p  v-for="(items,index) in introduceList" :key="index">{{items}}</p>
+            </div>
+            <div class="discountDetail">
+                <h1>【优惠详情】</h1>
+                <p v-for="(item,index) in discountDetailList" :key="index"><i class="circular">{{index+1}}</i>{{item}}</p>
+            </div>
     </div>
 </template>
 <script>
 export default {
     created(){
         this.phoneHeight = window.screen.height + 'px';
-        this.getData()
+        this.getData(0)
     },
     data(){
         return{
@@ -26,20 +28,31 @@ export default {
            introduceList:null,
            discountDetailList:null,
            headImage:null,
-           headDiscount:null
+           headDiscount:null,
+           list:[]
         }
     },
     methods:{
-        back(){
-            this.$router.go(-1);
+        changeBtn(index){
+            this.getData(index);
         },
-        getData(){
-            let DataList = this.$route.query;
-            this.introduceList = DataList.explain.split(";");
-            this.discountDetailList =  DataList.detial.split("。");
-            this.headImage = DataList.businessHead[0].url;
-            this.headDiscount =  DataList.discount;
+        // back(){
+        //     this.$router.go(-1);
+        // },
+        getData(index){
+            this.list = this.$route.query.businessHead;
+            console.log(this.list)
+            this.introduceList = this.list[index].explain.split(";");
+            this.discountDetailList =  this.list[index].detial.split("。");
+            // this.headImage = this.list[0].url;
+            // console.log(this.headImage)
+            this.headDiscount =  this.list[index].discount;
         }
     }
 }
 </script>
+<style>
+    .van-image{
+        height:240px;
+    }
+</style>
--- web/meixiWeb/src/components/public/tabar.vue
+++ web/meixiWeb/src/components/public/tabar.vue
@@ -1,8 +1,14 @@
 <template>
     <div>
         <div class="tabar">
-            <van-tabbar v-model="active" route>
+            <van-tabbar v-model="active" route @change="onChange">
             <van-tabbar-item to="/Index" name="home"    icon="vip-card">首页</van-tabbar-item>
+           <van-tabbar-item  @click="gemO=!gemO" name="gem-o"  icon="gem-o">梅溪半岛
+               <ul class="gemO" v-show="gemO">
+                   <li>最新活动</li>
+                   <li>样板间鉴赏</li>
+               </ul>
+           </van-tabbar-item>
             <van-tabbar-item to="/Business" name="friends" icon="friends">合作商家</van-tabbar-item>
             </van-tabbar>
         </div>
@@ -13,10 +19,15 @@ export default {
      data() {
     return {
       active: 'home',
+      gemO:false,
     }
   },
   methods:{
-      
+    onChange(index){
+        if(index!="gem-o"){
+            this.gemO=false;
+        }
+    }
   }
 }
 </script>
